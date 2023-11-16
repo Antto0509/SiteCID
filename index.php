4716937754d190d37203f6bbc955ae5e22d97235
Antoine Coutreel <114882531+Antto0509@users.noreply.github.com>
2023-11-16 09:22:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cercle des Informaticiens Dispersés</title>
+    <title>Document</title>
 </head>
 <body>
     

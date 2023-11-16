3bf86b5cfa8ece05d303bbde77c9011fbe23997b
Antto <coutreelantoine@gmail.com>
2023-11-16 11:08:29 +0100

    Update index.php & update imgs

--- index.php
+++ index.php
@@ -8,6 +8,23 @@
 <body>
     <head>
         <img src="assets/imgs/logo.png" alt="">
+        <div class='hamburger'>&#9776;</div>
+        <img id="search-icon" src="assets/imgs/loupe.png" alt="Loupe">
+        <input type="search" id="site-search" name="Rechercher..."/>
+        <img id="clear-icon" src="assets/imgs/croix.png" alt="Effacer">
+        <img src="assets/imgs/mail.png" alt="">
+        <img src="assets/imgs/compte.png" alt="">
     </head>
+    <h2>Présentation</h2>
+    <div>
+        <h3>Mot du président :</h3>
+        <p>Loremp ipsum</p>
+        <p>Je suis [Nom et Prénom], le/la Président.e de l'association du Cercle des Informaticiens Dispersés (CID). Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Sed sed arcu neque. Vestibulum id luctus dui, ut pharetra mi. Fusce tincidunt dolor nec pulvinar 
+            condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam mollis, turpis eget 
+            pharetra eleifend, libero ipsum commodo leo, in placerat leo enim.</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+    </div>
 </body>
 </html>

1f128985f2b532fb19af2d397d73e50d1f160dbb
Antto <coutreelantoine@gmail.com>
2023-11-21 10:39:56 +0100

    Update index.php & style.css

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cercle des Informaticiens Dispersés</title>
     <link rel="stylesheet" href="css/style.css">
+    <title>Cercle des Informaticiens Dispersés</title>
 </head>
 <body>
     <header>
@@ -43,16 +43,25 @@
         <button class="btn-mail"><img class="mail" src="assets/imgs/mail.png" alt=""></button>
         <button class="btn-compte"><img class="compte" src="assets/imgs/compte.png" alt=""></button>
     </header>
-    <h2>Présentation</h2>
-    <div>
-        <h3>Mot du président :</h3>
-        <p>Loremp ipsum</p>
-        <p>Je suis [Nom et Prénom], le/la Président.e de l'association du Cercle des Informaticiens Dispersés (CID). Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit. Sed sed arcu neque. Vestibulum id luctus dui, ut pharetra mi. Fusce tincidunt dolor nec pulvinar 
-            condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam mollis, turpis eget 
-            pharetra eleifend, libero ipsum commodo leo, in placerat leo enim.</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
+
+    <h1>Présentation</h1>
+    <div class="box-prez">
+        <div class="col1">
+            <h2>Mot du président :</h2>
+            <p>Loremp ipsum</p>
+            <p>Je suis [Nom et Prénom], le/la Président.e de l'association du Cercle des Informaticiens Dispersés (CID). Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. Sed sed arcu neque. Vestibulum id luctus dui, ut pharetra mi. Fusce tincidunt dolor nec pulvinar 
+                condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam mollis, turpis eget 
+                pharetra eleifend, libero ipsum commodo leo, in placerat leo enim.</p>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum</p>
+        </div>
+        <div class="col2">
+            <img src="" alt="">
+            <p>Président</p>
+            <img src="" alt="">
+            <p>Secrétaire</p>
+        </div>
     </div>
 </body>
 </html>

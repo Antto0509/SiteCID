bb8289182f8f1e1f400f5d323182714240a8412b
Antto <coutreelantoine@gmail.com>
2023-11-28 19:18:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -69,10 +69,16 @@
     <footer>
         <div class="col1-foot">
             <h3>Nous contacter :</h3>
-            <img class="tel-foot" src="assets/imgs/tel.png" alt="">
-            <a href="tel:0123456789">01.23.45.67.89</a>
-            <img class="mail-foot" src="assets/imgs/mail.png" alt="">
-            <a href="mailto:notre-mail@gmail.com?subject=Sujet du message">notre-mail@gmail.com</a>
+            <div class="contact-foot">
+                <div class="tel-foot">
+                    <img class="img-tel-foot" src="assets/imgs/tel.png" alt="">
+                    <a href="tel:0123456789">01.23.45.67.89</a>
+                </div>
+                <div class="mail-foot">
+                    <img class="img-mail-foot" src="assets/imgs/mail.png" alt="">
+                    <a href="mailto:notre-mail@gmail.com?subject=Sujet du message">notre-mail@gmail.com</a>
+                </div>
+            </div>
         </div>
         <div class="col2-foot">
             <h3>Informations :</h3>

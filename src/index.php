176a4bfda2b420aa3f199a3f18955023090b05b1
Antto <coutreelantoine@gmail.com>
2023-12-02 15:35:21 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Cercle des Informaticiens Dispersés</title>
+    <title>Accueil - Cercle des Informaticiens Dispersés</title>
 </head>
 <body>
     <?php include "header.php"?>

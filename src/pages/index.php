c7ff527ec6705fce2871a27d2900ac9e7969ad81
Antto <coutreelantoine@gmail.com>
2023-12-02 22:45:34 +0100

    Move footer.php, header.php and index.php in pages

---
+++ src/pages/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Accueil - Cercle des Informaticiens Dispersés</title>
+</head>
+<body>
+    <?php include "header.php"?>
+
+    <h1>Présentation</h1>
+    <div class="box-prez">
+        <div class="col1-prez">
+            <h2>Mot du président :</h2>
+            <p>Loremp ipsum</p>
+            <p>Je suis Nathalie Demazeux, la Présidente de l'association du Cercle des Informaticiens Dispersés (CID). Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. Sed sed arcu neque. Vestibulum id luctus dui, ut pharetra mi. Fusce tincidunt dolor nec pulvinar 
+                condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam mollis, turpis eget 
+                pharetra eleifend, libero ipsum commodo leo, in placerat leo enim.</p>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum</p>
+        </div>
+        <div class="col2-prez">
+            <img src="" alt="">
+            <p>Président</p>
+            <p>Secrétaire</p>
+            <img src="" alt="">
+        </div>
+    </div>
+
+    <h1>Annonces des étudiants</h1>
+    <button>Ajouter un événement</button>
+
+    <?php include "footer.php"?>
+</body>
+</html>
+

61077316a99e1c620cbf36b78d9e2e758965bc58
Antto <coutreelantoine@gmail.com>
2023-12-02 15:35:30 +0100

    Update login.php

---
+++ src/pages/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion - Cercle des Informaticiens Dispersés</title>
+</head>
+<body>
+    
+</body>
+</html>

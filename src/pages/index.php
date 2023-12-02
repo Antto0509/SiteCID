b88a78f7b4d2bfe384563105a606dc420e33fd2b
Antto <coutreelantoine@gmail.com>
2023-12-02 23:03:44 +0100

    Update index.php

--- src/pages/index.php
+++ src/pages/index.php
@@ -13,17 +13,17 @@
     <div class="box-prez">
         <div class="col1-prez">
             <h2>Mot du président :</h2>
-            <p>Loremp ipsum</p>
+            <p>Bonjour à tous,</p>
             <p>Je suis Nathalie Demazeux, la Présidente de l'association du Cercle des Informaticiens Dispersés (CID). Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit. Sed sed arcu neque. Vestibulum id luctus dui, ut pharetra mi. Fusce tincidunt dolor nec pulvinar 
                 condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam mollis, turpis eget 
                 pharetra eleifend, libero ipsum commodo leo, in placerat leo enim.</p>
-            <p>Lorem ipsum</p>
-            <p>Lorem ipsum</p>
+            <p>À bientôt,</p>
+            <p>Nathalie Demazeux</p>
         </div>
         <div class="col2-prez">
             <img src="" alt="">
-            <p>Président</p>
+            <p>Présidente</p>
             <p>Secrétaire</p>
             <img src="" alt="">
         </div>

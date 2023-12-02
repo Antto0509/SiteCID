230f41b97378bad95b1d459d19c230123877246a
Antto <coutreelantoine@gmail.com>
2023-12-02 22:45:43 +0100

    Update login.php

--- src/pages/login.php
+++ src/pages/login.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Connexion - Cercle des Informaticiens Dispersés</title>
 </head>
 <body>
-    
+    <?php include "header.php"?>
+
+    <?php include "footer.php"?>
 </body>
 </html>

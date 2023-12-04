175b195cb1d1561c97364ecf5e46f108403bfa94
Toinoup80 <127845250+Toinoup80@users.noreply.github.com>
2023-12-04 16:49:34 +0100

    Ajout page de connexion login

--- src/pages/login.php
+++ src/pages/login.php
@@ -3,13 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/connection.css">
     <title>Connexion - Cercle des Informaticiens Dispersés</title>
 </head>
 <body>
     <!-- Page de connexion (identifiants ou e-mail + mdp) -->
     <?php include "header.php"?>
+    <main class="conteneur-connexion">
+        <header class="titre-connexion">
+            <p>Se connecter</p>
+        </header>
+        <section class="saisie-infos-connexion">
+            <form action="" method="post">
+                <div class="groupe-input">
+                    <label for="identifiant">Identifiant</label>
+                    <input type="text" id="identifiant" name="adresse_email">
+                </div>
 
+                <div class="groupe-input">
+                    <label for="email">E-mail</label>
+                    <input type="email" id="email" name="adresse_email">
+                </div>
+
+                <div class="groupe-input">
+                    <label for="mdp">Mot de passe</label>
+                    <input type="password" id="mdp" name="mot_de_passe">
+                    
+                </div>    
+                <button type="submit">Connexion</button>
+            </form>
+            <div class="texte-connexion">
+                <p>Pas de compte ? <a href="#">Créer un compte</a></p>
+            </div>
+        </section>
+    </main>
     <?php include "footer.php"?>
 </body>
 </html>

40ffff6be275efed8ef4236c8ea86c159443d6f8
Antto <coutreelantoine@gmail.com>
2023-12-05 19:21:06 +0100

    Update register.php

--- src/pages/register.php
+++ src/pages/register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/connection.css">
+    <link rel="stylesheet" href="../css/connexion.css">
     <title>Inscription - Cercle des Informaticiens Dispersés</title>
 </head>
 <body>

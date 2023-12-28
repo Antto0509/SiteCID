d364fe19d4dbf36d132582913d0295d5ed64e039
desmarquet <39632693+desmarquet@users.noreply.github.com>
2023-12-28 19:12:54 +0100

    modif description

--- src/pages/infoU.php
+++ src/pages/infoU.php
@@ -29,10 +29,40 @@
             <span><?php echo $ligne['adresse_email']; ?></span>
         </div>
 
-        <div class="form-container descrition">
-            <label for="description" class="label">description :</label>
-            <span><?php echo $ligne['description']; ?></span>
-        </div>
+        <form action="script.php" method="post">
+            <div>
+                <label for="description">Description :</label>
+                <textarea name="description" id="description" rows="4" cols="50"></textarea>
+            </div>
+    
+            <button type="submit" name="ajouter">Ajouter</button>
+            <button type="submit" name="enregistrer">Enregistrer</button>
+            <button type="submit" name="supprimer">Supprimer</button>
+            <?php
+            // Initialisez une session PHP pour stocker la description
+            session_start();
+
+            // Vérifiez si la variable de session pour la description existe
+            if (!isset($_SESSION['description'])) {
+                $_SESSION['description'] = '';
+            }
+
+            // Vérifiez quel bouton a été cliqué
+            if (isset($_POST['ajouter'])) {
+                // Ajoutez la nouvelle description à la variable de session
+                $_SESSION['description'] .= $_POST['description'] . "\n";
+            } elseif (isset($_POST['enregistrer'])) {
+                // Vous pouvez implémenter ici la logique pour enregistrer la description, par exemple via une base de données
+                echo 'Description enregistrée : ' . $_SESSION['description'];
+            } elseif (isset($_POST['supprimer'])) {
+                // Réinitialisez la variable de session pour la description
+                $_SESSION['description'] = '';
+            }
+
+            // Redirigez l'utilisateur vers la page d'origine
+            header('Location: infoU.php');
+            ?>
+        </form>
         
     </section>
     <?php include "footer.php"?>
